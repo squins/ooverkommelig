65bf91622e71d2221b9853fa0993a3865eada4d8
Johan Stuyts <johanstuyts@squins.com>
2016-12-06 21:47:39 +0100

    Removed obsolete function.

--- src/main/kotlin/org/ooverkommelig/OOverkommelig.kt
+++ src/main/kotlin/org/ooverkommelig/OOverkommelig.kt
@@ -3,11 +3,3 @@ package org.ooverkommelig
 fun <TValue : Any> req(definition: Definition<TValue>) = definition.get()
 
 fun <TValue> opt(definition: Definition<TValue>) = definition.get()
-
-inline fun <reified TInterface : Any> i(): Class<TInterface> {
-    val result = TInterface::class.java
-
-    check(result.isInterface, { "Type: ${result.name}, is not an interface." })
-
-    return result
-}
