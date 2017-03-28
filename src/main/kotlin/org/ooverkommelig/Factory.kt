0a96bf1f3994c42ba7f4777bd54965c7e10e7016
Johan Stuyts <j.stuyts@javathinker.com>
2017-03-28 23:22:23 +0200

    Moved "provideDelegate" operator up the hierarchy so that it is still invoked when lifecycle methods are used.

--- src/main/kotlin/org/ooverkommelig/Factory.kt
+++ src/main/kotlin/org/ooverkommelig/Factory.kt
@@ -2,13 +2,11 @@ package org.ooverkommelig
 
 import org.ooverkommelig.definition.FactoryDefinition
 import org.ooverkommelig.definition.ObjectCreatingDefinitionDelegate
-import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class Factory<TObject>(internal val create: () -> TObject) : ObjectCreatingDefinitionDelegate<Definition<TObject>, TObject>() {
-    operator fun provideDelegate(owner: SubGraphDefinition, property: KProperty<*>): ReadOnlyProperty<SubGraphDefinition, Definition<TObject>> {
+    override fun registerPropertyIfNeeded(owner: SubGraphDefinition, property: KProperty<*>) {
         owner.addDefinitionProperty(property, false)
-        return this
     }
 
     override fun createDefinition(owner: SubGraphDefinition, name: String): Definition<TObject> = FactoryDefinition(owner, name, this)
--- src/main/kotlin/org/ooverkommelig/Singleton.kt
+++ src/main/kotlin/org/ooverkommelig/Singleton.kt
@@ -2,13 +2,11 @@ package org.ooverkommelig
 
 import org.ooverkommelig.definition.ObjectCreatingDefinitionDelegate
 import org.ooverkommelig.definition.SingletonDefinition
-import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class Singleton<TObject>(internal val create: () -> TObject) : ObjectCreatingDefinitionDelegate<Definition<TObject>, TObject>() {
-    operator fun provideDelegate(owner: SubGraphDefinition, property: KProperty<*>): ReadOnlyProperty<SubGraphDefinition, Definition<TObject>> {
+    override fun registerPropertyIfNeeded(owner: SubGraphDefinition, property: KProperty<*>) {
         owner.addDefinitionProperty(property, true)
-        return this
     }
 
     override fun createDefinition(owner: SubGraphDefinition, name: String): Definition<TObject> = SingletonDefinition(owner, name, this)
