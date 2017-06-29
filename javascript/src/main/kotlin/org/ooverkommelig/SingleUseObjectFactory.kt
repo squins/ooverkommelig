d328ba67f0a6611ad4b56329cb738be9750e7d2b
Johan Stuyts <j.stuyts@javathinker.com>
2017-06-29 06:06:28 +0200

    Restored compatibility by the introduction of secondary constructors.

--- javascript/src/main/kotlin/org/ooverkommelig/SingleUseObjectFactory.kt
+++ javascript/src/main/kotlin/org/ooverkommelig/SingleUseObjectFactory.kt
@@ -1,6 +1,8 @@
 package org.ooverkommelig
 
 impl abstract class SingleUseObjectFactory<out TObject : Any>(provided: ProvidedBase, objectGraphConfiguration: ObjectGraphConfiguration) : SingleUseObjectFactoryCommon<TObject>(provided, objectGraphConfiguration) {
+    constructor (provided: ProvidedBase) : this(provided, ObjectGraphConfiguration())
+
     impl fun <TResult> createAndUse(block: (TObject) -> TResult) =
             Graph().run {
                 try {
