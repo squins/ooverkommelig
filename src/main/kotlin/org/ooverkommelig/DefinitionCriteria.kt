e98e0eb936c953903b2a6ab702b6bfa75e862f35
Johan Stuyts <j.stuyts@javathinker.com>
2017-08-18 11:17:51 +0200

    Improved notation of types.

--- src/main/kotlin/org/ooverkommelig/DefinitionCriteria.kt
+++ src/main/kotlin/org/ooverkommelig/DefinitionCriteria.kt
@@ -1,3 +1,5 @@
 package org.ooverkommelig
 
-class DefinitionCriteria<TObject>(val objectType: ObjectType<TObject>, val mustReturnSameObjectForAllRetrievals: Boolean = false)
+class DefinitionCriteria<TObject>(private val functionWithResultDefininingType: () -> TObject, val mustReturnSameObjectForAllRetrievals: Boolean = false) {
+    fun getType() = functionWithResultDefininingType.asKType()
+}
