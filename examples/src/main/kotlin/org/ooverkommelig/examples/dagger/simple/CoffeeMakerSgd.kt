a4589c0431fac51d46e98684eb5d2f152826ad94
Johan Stuyts <j.stuyts@javathinker.com>
2017-09-07 20:52:58 +0200

    Switched to "D" instead of "Definition" in examples.

--- examples/src/main/kotlin/org/ooverkommelig/examples/dagger/simple/CoffeeMakerSgd.kt
+++ examples/src/main/kotlin/org/ooverkommelig/examples/dagger/simple/CoffeeMakerSgd.kt
@@ -1,6 +1,6 @@
 package org.ooverkommelig.examples.dagger.simple
 
-import org.ooverkommelig.Definition
+import org.ooverkommelig.D
 import org.ooverkommelig.Once
 import org.ooverkommelig.ProvidedBase
 import org.ooverkommelig.SubGraphDefinition
@@ -8,8 +8,8 @@ import org.ooverkommelig.req
 
 class CoffeeMakerSgd(provided: Provided) : SubGraphDefinition(provided) {
     interface Provided : ProvidedBase {
-        fun heater(): Definition<Heater>
-        fun pump(): Definition<Pump>
+        fun heater(): D<Heater>
+        fun pump(): D<Pump>
     }
 
     val coffeeMaker by Once { CoffeeMaker(lazy { req(provided.heater()) }, req(provided.pump())) }
