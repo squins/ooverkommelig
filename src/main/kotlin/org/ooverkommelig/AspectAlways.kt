3bb3e397a3f1adf2eef8fe4d62d6b351a94fa38f
Johan Stuyts <j.stuyts@javathinker.com>
2017-09-08 18:24:43 +0200

    Deleted files added to master, and that were still there after the merge.

--- src/main/kotlin/org/ooverkommelig/AspectAlways.kt
+++
@@ -1,9 +0,0 @@
-package org.ooverkommelig
-
-import org.ooverkommelig.definition.AspectAlwaysDefinition
-import org.ooverkommelig.definition.AspectDelegate
-
-open class AspectAlways<TInterface>(override val create: (Class<TInterface>, Definition<TInterface>) -> TInterface) :
-        AspectDelegate<TInterface>() {
-    override fun createDefinition(owner: SubGraphDefinition, name: String) = AspectAlwaysDefinition<TInterface>(owner, name, this)
-}
