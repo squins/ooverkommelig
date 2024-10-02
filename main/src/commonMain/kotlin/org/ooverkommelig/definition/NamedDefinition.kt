39f73caa5c487b3cf840935c7d8af9cc062f99b3
Johan Stuyts <j.stuyts@javathinker.com>
2024-10-02 09:55:21 +0200

    Made the name of a definition accessible to clients.

---
+++ main/src/commonMain/kotlin/org/ooverkommelig/definition/NamedDefinition.kt
@@ -0,0 +1,5 @@
+package org.ooverkommelig.definition
+
+interface NamedDefinition<TObject> {
+    val name: String
+}
--- main/src/commonMain/kotlin/org/ooverkommelig/definition/ObjectCreatingDefinition.kt
+++ main/src/commonMain/kotlin/org/ooverkommelig/definition/ObjectCreatingDefinition.kt
@@ -2,9 +2,8 @@ package org.ooverkommelig.definition
 
 import org.ooverkommelig.SubGraphDefinition
 
-internal interface ObjectCreatingDefinition<TObject> {
+internal interface ObjectCreatingDefinition<TObject> : NamedDefinition<TObject> {
     val owner: SubGraphDefinition
-    val name: String
     val delegate: ObjectCreatingDefinitionDelegate<*, TObject>
 
     fun handleCreation(argument: Any? = null, creator: () -> TObject) = owner.handleCreation(this, argument, creator)
